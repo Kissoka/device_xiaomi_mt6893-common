52396a642ad02601b27d081bab9a3dd8a6cfb421
kissoka <orangefoxci73@gmail.com>
2023-07-20 21:06:06 +0200

    mt6893: drop libhidl
    outdated and dropped from lineage hardware_compat repo

--- mt6893.mk
+++ mt6893.mk
@@ -144,8 +144,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0_system \
-    android.hidl.manager@1.0_system \
     android.hidl.memory.block@1.0.vendor \
     android.hidl.memory.block@1.0 \
     libhwbinder \
