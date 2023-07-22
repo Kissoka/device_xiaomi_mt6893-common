1f5e2f9bba86ef238060a369ca70037c8266460f
bengris32 <bengris32@protonmail.ch>
2023-07-22 03:13:30 +0200

    mt6893: Build USB service from source

--- mt6893.mk
+++ mt6893.mk
@@ -309,8 +309,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1.vendor \
-    android.hardware.usb.gadget@1.1.vendor
+    android.hardware.usb@1.3-service-mediatekv2
 
 # Vendor service
 PRODUCT_PACKAGES += \
